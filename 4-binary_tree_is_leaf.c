3af483552665b7547e7e5b00e12d62f289ba2f6a
Chidiebube-web <chidiebubeonyekwelu@gmail.com>
2022-08-25 10:27:29 +0000

    file updated

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,4 +1,4 @@
-include "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
@@ -12,5 +12,3 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	return ((!node || node->left || node->right) ? 0 : 1);
 }
-Footer
-
